4e63e76af55152b34ea0ae451790cb5c0c3f08f4
Jeremi <kdrisaac42@gmail.com>
2020-05-16 16:08:12 +0200

    hotelService update

--- BookingWebApp/src/app/core/services/hotel.service.ts
+++ BookingWebApp/src/app/core/services/hotel.service.ts
@@ -108,6 +108,71 @@ export class HotelService {
       );
   }
 
+  getRooms(hotelId: string): Observable<RoomModel[]> {
+    const url = this.backendUrl + `hotels/${hotelId}/rooms`;
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      withCredentials: true
+    };
+    return this.http.get<RoomModel[]>(url, httpOptions)
+      .pipe(
+        tap(_ => console.log('[HotelService] Fetching rooms...')),
+        catchError(this.handleError<RoomModel[]>('getRooms', []))
+      );
+  }
+
+  getRoom(hotelId: string, roomNumber: number): Observable<RoomModel> {
+    const url = this.backendUrl + `hotels/${hotelId}/rooms/${roomNumber}`;
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      withCredentials: true
+    };
+    return this.http.get<RoomModel>(url, httpOptions)
+      .pipe(
+        tap(_ => console.log(`[HotelService] Fetching room for roomNumber: + ${roomNumber}`)),
+        catchError(this.handleError<RoomModel>('getRoom', {} as RoomModel))
+      );
+  }
+
+  addRoom(dto: RoomModel, hotelId: string): Observable<RoomModel> {
+    const url = this.backendUrl + `hotels/${hotelId}/rooms`;
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      withCredentials: true
+    };
+    return this.http.post<RoomModel>(url, JSON.stringify(dto), httpOptions)
+      .pipe(
+        tap(_ => console.log(`[HotelService] Adding room: + ${dto}`)),
+        catchError(this.handleError<RoomModel>('addRoom', {} as RoomModel))
+      );
+  }
+
+  updateRoom(dto: RoomModel, hotelId: string): Observable<RoomModel> {
+    const url = this.backendUrl + `hotels/${hotelId}/rooms/${dto._id}`;
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      withCredentials: true
+    };
+    return this.http.put<RoomModel>(url, JSON.stringify(dto), httpOptions)
+      .pipe(
+        tap(_ => console.log(`[HotelService] Updating room: + ${dto}`)),
+        catchError(this.handleError<RoomModel>('updateRoom', {} as RoomModel))
+      );
+  }
+
+  deleteRoom(hotelId: string, id: string): Observable<boolean> {
+    const url = this.backendUrl + `hotels/${hotelId}/rooms/${id}`;
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      withCredentials: true
+    };
+    return this.http.delete<boolean>(url, httpOptions)
+      .pipe(
+        tap(_ => console.log(`[HotelService] Deleting room (id): + ${id}`)),
+        catchError(this.handleError<boolean>('deleteRoom', false))
+      );
+  }
+
   bookRooms(idList: string[]): Observable<boolean> {
     return of(true);
   }
